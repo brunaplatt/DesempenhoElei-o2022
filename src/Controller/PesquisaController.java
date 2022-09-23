44a503ee26619b718543fe96b7017bdec9f17329
Bruna Platt <111389173+brunaplatt@users.noreply.github.com>
2022-09-23 16:17:45 -0300

    Update PesquisaController.java

--- src/Controller/PesquisaController.java
+++ src/Controller/PesquisaController.java
@@ -8,23 +8,23 @@ import Tabelas.Pesquisa;
 public class PesquisaController {
 
     public void salvar(Pesquisa pesquisa) throws Exception{
-        if (pesquisa.getFormatopesquisa() == null || pesquisa.getFormatopesquisa().trim().equals("")) {
-            throw new Exception("Descrição inválida");
+        if (pesquisa.getDatapesquisa() == null || pesquisa.getInstitutopesquisa() == null) {
+            throw new Exception("Dados incompletos!");
         }
         PesquisaDao.getInstance().salvar(pesquisa);
 
     }
 
     public void atualizar(Pesquisa pesquisa) throws Exception {
-        if (pesquisa.getFormatopesquisa() == null || pesquisa.getFormatopesquisa().trim().equals("")) {
-            throw new Exception("Descrição inválida");
+    	 if (pesquisa.getDatapesquisa() == null || pesquisa.getInstitutopesquisa() == null) {
+            throw new Exception("Dados incompletos!");
         }
         PesquisaDao.getInstance().atualizar(pesquisa);
     }
 
     public void excluir(int idPesquisa) throws Exception {
         if (idPesquisa == 0) {
-            throw new Exception("Erro");
+            throw new Exception("Nenhuma pesquisa encontrada.");
 
         }
         PesquisaDao.getInstance().excluir(idPesquisa);
